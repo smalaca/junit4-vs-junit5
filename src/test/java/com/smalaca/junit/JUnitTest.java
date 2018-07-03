75a2a56489ce1bfce4bfa7db11c8ea37af08f6ec
smalaca <ab13.krakow@gmail.com>
2018-07-03 08:39:34 +0200

    pom dependencies and JUnitTest fix

--- src/test/java/com/smalaca/junit/JUnitTest.java
+++ src/test/java/com/smalaca/junit/JUnitTest.java
@@ -1,8 +1,9 @@
 package com.smalaca.junit;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JUnitTest {
     @Test
